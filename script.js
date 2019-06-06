19cf4e20d5a298cc045c53e4586356016742daaa
Shizzyq <44840391+Shizzyq@users.noreply.github.com>
2019-06-06 21:04:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+$("#about-btn").click(function(){
+    $("#landing").fadeOut(300);
+    $("#about").fadeIn(1500);   
+  });
+
+
+  $("#logo").click(function(){
+    $("#about").fadeOut(500);
+    $("#landing").fadeIn(1000);   
+  });
+
+
+  $("#about-btn").click(function(){
+    $("#about").fadeIn(1000);   
+  });
